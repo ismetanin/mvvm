115c8179a1dd5cc1da14e44379669f5b0d879c92
Ivan Smetanin <smetanin23@yandex.ru>
2019-04-07 13:05:07 +0300

    Initial Commit

---
+++ mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by Ivan Smetanin on 07/04/2019.
+//  Copyright © 2019 Ivan Smetanin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
