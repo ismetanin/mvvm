f002b283c42e102693d0691c0f551d55b6dca940
Ivan Smetanin <smetanin23@yandex.ru>
2019-05-12 15:00:19 +0300

    Remove unused code, rename extension file

--- mvvm/Library/Extensions/Rx/Observable.swift
+++ mvvm/Library/Extensions/Rx/Observable.swift
@@ -1,5 +1,5 @@
 //
-//  Observable+Ext.swift
+//  Observable.swift
 //  mvvm
 //
 //  Created by Ivan Smetanin on 07/04/2019.
@@ -10,39 +10,12 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-extension ObservableType where E == Bool {
-
-    /// Boolean not operator
-    public func not() -> Observable<Bool> {
-        return self.map(!)
-    }
-
-}
-
-extension SharedSequenceConvertibleType {
-
-    func mapToVoid() -> SharedSequence<SharingStrategy, Void> {
-        return map { _ in }
-    }
-
-}
-
 extension ObservableType {
-
-    func catchErrorJustComplete() -> Observable<E> {
-        return catchError { _ in
-            return Observable.empty()
-        }
-    }
 
     func asDriverOnErrorJustComplete() -> Driver<E> {
         return asDriver { error in
             return Driver.empty()
         }
-    }
-
-    func mapToVoid() -> Observable<Void> {
-        return map { _ in }
     }
 
 }
